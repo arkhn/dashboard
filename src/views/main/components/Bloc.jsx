e5f64798278ff304d696a35e86ff13e5501eadef
alexis <alexisthual@gmail.com>
2019-05-08 19:30:10 +0200

    Bigger value for late block

--- src/views/main/components/Bloc.jsx
+++ src/views/main/components/Bloc.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default () => {
   return (
     <div>
-      <div>Retard cumulé: 10 heures</div>
+      <div>
+        Retard cumulé: <span className="value">10 heures</span>
+      </div>
     </div>
   );
 };
