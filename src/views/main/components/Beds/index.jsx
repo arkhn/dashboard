0c04cd23fbe5ab2cb3321a6367de8032cd87dfea
alexis <alexisthual@gmail.com>
2019-05-08 19:26:51 +0200

    Add progressbars to current modules

--- src/views/main/components/Beds/index.jsx
+++ src/views/main/components/Beds/index.jsx
@@ -1,3 +1,4 @@
+import { ProgressBar } from "@blueprintjs/core";
 import React from "react";
 
 export default ({ beds, totalBeds }) => {
@@ -5,11 +6,25 @@ export default ({ beds, totalBeds }) => {
     <div>
       <div>
         Total: {beds} / {totalBeds}
+        <ProgressBar
+          intent={"warning"}
+          stripes={false}
+          value={beds / totalBeds}
+        />
       </div>
       <h3>Critiques</h3>
-      <div>Surveillance continue: 12/15</div>
-      <div>Réanimation: 5/8</div>
-      <div>Soins intensifs: 2/4</div>
+      <div>
+        Surveillance continue: 12/15
+        <ProgressBar intent={"warning"} stripes={false} value={12 / 15} />
+      </div>
+      <div>
+        Réanimation: 5/8
+        <ProgressBar intent={"success"} stripes={false} value={5 / 8} />
+      </div>
+      <div>
+        Soins intensifs: 2/4
+        <ProgressBar intent={"success"} stripes={false} value={2 / 4} />
+      </div>
     </div>
   );
 };
--- src/views/main/components/Resources/index.jsx
+++ src/views/main/components/Resources/index.jsx
@@ -1,3 +1,4 @@
+import { ProgressBar } from "@blueprintjs/core";
 import React from "react";
 
 export default ({ ide, totalIde, aides, totalAides }) => {
@@ -6,16 +7,31 @@ export default ({ ide, totalIde, aides, totalAides }) => {
       <h3>IDE</h3>
       <div>
         Total: {ide}/{totalIde}
+        <ProgressBar intent={"danger"} stripes={false} value={ide / totalIde} />
       </div>
-      <div>Réanimation: 3/12</div>
+      <div>
+        Réanimation: 3/12
+        <ProgressBar intent={"success"} stripes={false} value={3 / 12} />
+      </div>
       <div>Ratio 1:5</div>
-      <div>Soins intensifs: 2/4</div>
+      <div>
+        Soins intensifs: 2/4
+        <ProgressBar intent={"success"} stripes={false} value={2 / 4} />
+      </div>
       <div>Ratio 1:3</div>
       <h3>Aide soignant(e)s</h3>
       <div>
         Total: {aides}/{totalAides}
+        <ProgressBar
+          intent={"warning"}
+          stripes={false}
+          value={aides / totalAides}
+        />
       </div>
-      <div>Réanimation: 9/31</div>
+      <div>
+        Réanimation: 9/31
+        <ProgressBar intent={"success"} stripes={false} value={9 / 31} />
+      </div>
       <div>Ratio 1:2</div>
     </div>
   );
